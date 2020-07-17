7ce036f3564fdf2cf028be77f9b97c726a5e36dc
Abhijeet6387 <abhijeet.s.mishra@gmail.com>
2020-07-17 18:21:32 +0530

    Single Page Applications Part 2+change in menu

--- src/components/DishdetailComponent.js
+++ src/components/DishdetailComponent.js
@@ -59,12 +59,8 @@ import { Link } from 'react-router-dom';
                         </div>                
                     </div>
                     <div className="row">
-                        <div className="col-12 col-md-5 m-1">
                             <RenderDish dish={props.dish}></RenderDish>
-                        </div>
-                        <div className="col-12 col-md-5 m-1">
                             <RenderComments comments={props.comments}></RenderComments>
-                        </div>
                     </div>
                 </div>
             );
