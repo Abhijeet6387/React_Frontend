f85defeb2d3e8470720fb024c6f26f26773bc6f4
Abhijeet6387 <abhijeet.s.mishra@gmail.com>
2020-08-15 18:24:30 +0530

    week 4 done

--- src/components/ContactComponent.js
+++ src/components/ContactComponent.js
@@ -1,30 +1,85 @@
-import React, { Component} from 'react';
-import { Breadcrumb, BreadcrumbItem, Button, Row, Col, Label } from 'reactstrap';
-import { Control, LocalForm,Errors} from 'react-redux-form';
+import React, { Component } from 'react';
+import { Breadcrumb, BreadcrumbItem,
+    Button, Row, Col, Label } from 'reactstrap';
+import { Control, Form, Errors, actions } from 'react-redux-form';
 import { Link } from 'react-router-dom';
-
-const required = (val) => val && val.length;
-const maxLength = (len) => (val) => !(val) || (val.length <= len);
-const minLength = (len) => (val) => val && (val.length >= len);
-const isNumber = (val) => !isNaN(Number(val));
-const validEmail = (val) => /^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}$/i.test(val);
-
-class Contact extends Component{
-
+import { baseUrl } from "../shared/baseUrl";
+class Contact extends Component {
     constructor(props) {
         super(props);
-
-        this.handleSubmit = this.handleSubmit.bind(this);
+        this.state = {
+            firstname: '',
+            lastname: '',
+            telnum: '',
+            email: '',
+            agree: false,
+            contactType: 'Tel.',
+            message: '',
+            touched: {
+                firstname: false,
+                lastname: false,
+                telnum: false,
+                email: false
+            }
         };
-
+        this.handleInputChange = this.handleInputChange.bind(this);
+        this.handleSubmit = this.handleSubmit.bind(this);
+        this.handleBlur = this.handleBlur.bind(this);
+    }
+    handleInputChange(event) {
+        const target = event.target;
+        const value = target.type === 'checkbox' ? target.checked : target.value;
+        const name = target.name;
+    
+        this.setState({
+          [name]: value
+        });
+    }
     handleSubmit(values) {
         console.log('Current State is: ' + JSON.stringify(values));
         alert('Current State is: ' + JSON.stringify(values));
+        this.props.postFeedback(values);
+        this.props.resetFeedbackForm();
         // event.preventDefault();
     }
+    handleBlur = (field) => (evt) =>{
+        this.setState({
+            touched: {...this.state.touched, [field]: true}
+        })
+    }
+    validate(firstname, lastname, telnum, email){
+        const errors = {
+            firstname: '',
+            lastname: '',
+            telnum: '',
+            email: ''
+        };
+        if(this.state.touched.firstname && firstname.length <= 3)
+            errors.firstname = "Firstname should be less than 3 characters";
+        else if(this.state.touched.firstname && firstname.length >= 10)
+            errors.firstname = "Firstname should be more than 10 characters";
 
+        if(this.state.touched.lastname && lastname.length <= 3)
+            errors.lastname = "Lastname should be less than 3 characters";
+        else if(this.state.touched.lastname && lastname.length >= 10)
+            errors.lastname = "Lastname should be more than 10 characters";
+        
+        const reg = /^\d+$/;
+        if(this.state.touched.telnum && !reg.test(telnum))
+            errors.telnum = "Tel num should contain only numbers";
+        
+        if(this.state.touched.email && email.split('').filter(x => x === '@').length !== 1)
+            errors.email = "Email should contain '@' sign";
 
-    render() {
+        return errors;
+    }
+    render(){
+        const errors = this.validate(this.state.firstname, this.state.lastname, this.state.telnum, this.state.email);
+        const required = (val) => val && val.length;
+        const maxLength = (len) => (val) => !(val) || (val.length <= len);
+        const minLength = (len) => (val) => val && (val.length >= len);
+        const isNumber = (val) => !isNaN(Number(val));
+        const validEmail = (val) => /^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}$/i.test(val);
         return(
             <div className="container">
                 <div className="row">
@@ -34,7 +89,7 @@ class Contact extends Component{
                     </Breadcrumb>
                     <div className="col-12">
                         <h3>Contact Us</h3>
-                        <hr></hr>
+                        <hr />
                     </div>                
                 </div>
                 <div className="row row-content">
@@ -63,12 +118,12 @@ class Contact extends Component{
                         </div>
                     </div>
                 </div>
-                <div className="row row-content">
-                   <div className="col-12">
-                      <h3>Send us your Feedback</h3>
-                   </div>
-                    <div className="col-12 col-md-9">
-                        <LocalForm onSubmit={(values) => this.handleSubmit(values)}>
+                        <div className="row row-content">
+                    <div className="col-12">
+                        <h3>Send us your Feedback</h3>
+                    </div>
+                        <div className="col-12 col-md-9">
+                        <Form model="feedback" onSubmit={(values) => this.handleSubmit(values)}>
                         <Row className="form-group">
                                 <Label htmlFor="firstname" md={2}>First Name</Label>
                                 <Col md={10}>
@@ -157,10 +212,47 @@ class Contact extends Component{
                                      />
                                 </Col>
                             </Row>
-                        </LocalForm>
-                    </div>
+                            <Row className="form-group">
+                                <Col md={{size: 6, offset: 2}}>
+                                    <div className="form-check">
+                                        <Label check>
+                                            <Control.checkbox model=".agree" name="agree"
+                                                className="form-check-input"
+                                                 /> {' '}
+                                                <strong>May we contact you?</strong>
+                                        </Label>
+                                    </div>
+                                </Col>
+                                <Col md={{size: 3, offset: 1}}>
+                                    <Control.select model=".contactType" name="contactType"
+                                        className="form-control">
+                                        <option>Tel.</option>
+                                        <option>Email</option>
+                                    </Control.select>
+                                </Col>
+                            </Row>
+                            <Row className="form-group">
+                                <Label htmlFor="message" md={2}>Your Feedback</Label>
+                                <Col md={10}>
+                                    <Control.textarea model=".message" id="message" name="message"
+                                        rows="12"
+                                        className="form-control" />
+                                </Col>
+                            </Row>
+                            <Row className="form-group">
+                                <Col md={{size:10, offset: 2}}>
+                                    <Button type="submit" color="primary">
+                                    Send Feedback
+                                    </Button>
+                                </Col>
+                            </Row>
+                        </Form>
+                        </div>
                 </div>
             </div>
+
+
+           
         );
     }
 }
